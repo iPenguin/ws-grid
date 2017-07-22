34872c8f8246c14c28b8fe1bae2dd058c34b0cb1
Brian C. Milco <bcmilco@gmail.com>
2017-07-21 23:34:02 -0700

    Add filter and modify test page.

--- test.js
+++ test.js
@@ -13,16 +13,16 @@ let columns = [
     { name: 'first_name', label: 'First Name', width: 200, align: 'left',   fixed: false, sort: 'string' },
     { name: 'last_name',  label: 'Last Name',  width: 200, align: 'left',   fixed: false, sort: 'string' },
     { name: 'age',        label: 'Age',        width: 100, align: 'right',  fixed: true,  sort: 'number' },
-    { name: 'height',     label: 'Height',     width: 100, align: 'center', fixed: true,  sort: 'custom', sort_function: sort_height },
-    { name: 'dob',        label: 'DOB',        width: 100, align: 'center', fixed: true,  sort: 'date',  },
+    { name: 'height',     label: 'Height',     width: 200, align: 'center', fixed: true,  sort: 'custom', sort_function: sort_height },
+    { name: 'dob',        label: 'DOB',        width: 200, align: 'center', fixed: true,  sort: 'date',  },
 ];
 
 let grid = new wsGrid( {
     id:           'grid',
     url:          'ws://localhost:8000',
     column_model: columns,
-    height:       200,
-    width:        1000,
+    height:       300,
+    width:        1200,
     events:       {
         click( row, column_name, data ) {
             console.log( "click", row, column_name );
@@ -56,6 +56,29 @@ grid.fill_grid( [
     { id: 114, first_name: 'Emma', last_name: 'Johnson', age: 48, height: '5\'3"', dob: '1982-12-03' },
     { id: 121, first_name: 'John', last_name: 'Smith',   age: 28, height: '5\'10"', dob: '1990-04-07' },
     { id: 122, first_name: 'Jane', last_name: 'Doe',     age: 26, height: '5\'6"', dob: '1998-02-05' },
-    { id: 123, first_name: 'Matt', last_name: 'Miller',  age: 45, height: '5\'9"', dob: '2001-04-30' },
     { id: 124, first_name: 'Emma', last_name: 'Johnson', age: 48, height: '5\'3"', dob: '2005-11-6'  },
+    { id: 123, first_name: 'Matt', last_name: 'Miller',  age: 45, height: '5\'9"', dob: '2001-04-30' },
 ] );
+
+
+let select = document.getElementById( 'filter_column' );
+//Create a drop down to select the column to filter on.
+columns.forEach( ( column ) => {
+    let holder = document.createElement( 'div' );
+    holder.innerHTML = `<option value="${column.name}">${column.label}</option>`;
+
+    select.append( holder.firstChild );
+} );
+
+let filter = document.getElementById( 'filter' );
+filter.onchange = () => {
+    grid.filter( [
+        {
+            field:    select.options[ select.selectedIndex ].value,
+            type:     'string',
+            operator: '==',
+            test:     filter.value,
+        }
+    ] );
+
+};
